8050089066fe63f64d0fcab8f66b36070b499b13
1-EXON <exon.indiv@gmail.com>
2023-01-18 18:25:56 +0900

    Update:

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
+import time
+import json
+import schedule
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+
+with open('settings.json') as f:
+    json_object = json.load(f)
 
 url = 'https://benedu.co.kr/'
-uid = ''
-upw = ''
+uid = json_object['uid']
+upw = json_object['upw']
 
 chrome_path ='chromedriver.exe'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("lang=ko_KR") # 한국어
 chrome_options.add_argument('window-size=1920x1080')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.get(url)
-driver.implicitly_wait(3)
+def job():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get(url)
+    driver.implicitly_wait(3)
 
-loginId = driver.find_element(By.CSS_SELECTOR, '#loginID')
-loginPw = driver.find_element(By.CSS_SELECTOR, '#loginPW')
-loginId.send_keys(uid)
-loginPw.send_keys(upw)
+    loginId = driver.find_element(By.CSS_SELECTOR, '#loginID')
+    loginPw = driver.find_element(By.CSS_SELECTOR, '#loginPW')
+    loginId.send_keys(uid)
+    loginPw.send_keys(upw)
 
-driver.find_element(By.CSS_SELECTOR, 'body > section > div.wrap > div.container.right > form > div > div.login-buttons > button:nth-child(1)').click()
+    driver.find_element(By.CSS_SELECTOR, 'body > section > div.wrap > div.container.right > form > div > div.login-buttons > button:nth-child(1)').click()
 
-time.sleep(10)
+schedule.every().day.at('10:30').do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
